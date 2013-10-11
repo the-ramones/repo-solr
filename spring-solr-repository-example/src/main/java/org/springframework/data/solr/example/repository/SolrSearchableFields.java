16128f09ad5bcae2661ba26b24a313290b24243a
Кулицкий <@ASU-2.ZSK.BY>
2013-10-12 01:09:51 +1200

    init2

--- spring-solr-repository-example/src/main/java/org/springframework/data/solr/example/repository/SolrSearchableFields.java
+++ spring-solr-repository-example/src/main/java/org/springframework/data/solr/example/repository/SolrSearchableFields.java
@@ -23,19 +23,21 @@ import org.springframework.data.solr.example.model.SearchableProduct;
  */
 public enum SolrSearchableFields implements Field {
 
-	ID(SearchableProduct.ID_FIELD), NAME(SearchableProduct.NAME_FIELD), PRICE(SearchableProduct.PRICE_FIELD), AVAILABLE(
-			SearchableProduct.AVAILABLE_FIELD), CATEGORY(SearchableProduct.CATEGORY_FIELD), WEIGHT(
-			SearchableProduct.WEIGHT_FIELD), POPULARITY(SearchableProduct.POPULARITY_FIELD);
+    ID(SearchableProduct.ID_FIELD),
+    NAME(SearchableProduct.NAME_FIELD),
+    PRICE(SearchableProduct.PRICE_FIELD),
+    AVAILABLE(SearchableProduct.AVAILABLE_FIELD),
+    CATEGORY(SearchableProduct.CATEGORY_FIELD),
+    WEIGHT(SearchableProduct.WEIGHT_FIELD),
+    POPULARITY(SearchableProduct.POPULARITY_FIELD);
+    private final String fieldName;
 
-	private final String fieldName;
+    private SolrSearchableFields(String fieldName) {
+        this.fieldName = fieldName;
+    }
 
-	private SolrSearchableFields(String fieldName) {
-		this.fieldName = fieldName;
-	}
-
-	@Override
-	public String getName() {
-		return fieldName;
-	}
-
+    @Override
+    public String getName() {
+        return fieldName;
+    }
 }
